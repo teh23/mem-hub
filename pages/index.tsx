73c1be68a421fc2e4e7c7ce0aa996ac04dbdca8b
Andrzej Franek <andrzejfranek@hotmail.com>
2021-07-08 23:03:54 +0200

    add superjson serialistion; fix gitignore

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,37 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { PrismaClient } from '@prisma/client'
+import { isError } from 'util'
 
-export default function Home() {
+interface IUser {
+    id: Number
+    username: String
+    password: String
+    createAt: Date
+}
 
+export default function Home({ users }: { users: IUser }) {
+    console.log(users)
 
-  return (
-    <>
-      <Head>
-        <title></title>
-      </Head>
+    return (
+        <>
+            <Head>
+                <title>a</title>
+            </Head>
 
-    </>
-  )
+        </>
+    )
+}
+
+export async function getStaticProps() {
+    const prisma = new PrismaClient()
+    const user = await prisma.user.findMany()
+
+    return {
+        props: {
+            users: user
+        }
+
+    }
+
 }
