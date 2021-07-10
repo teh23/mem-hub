d5f6949bd30d81a5ae0008d645c4fd9fc2f63ec6
Andrzej Franek <andrzejfranek@hotmail.com>
2021-07-10 17:38:12 +0200

    fix layout return type

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Layout = ({ children }: { children: React.ReactElement }) => {
+const Layout = ({ children }: { children: React.ReactElement[] }) => {
     return (
         <div className="container">
             {children}
