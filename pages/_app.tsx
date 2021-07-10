ce123017659cdf69d8f265fb814618273728c1de
Andrzej Franek <andrzejfranek@hotmail.com>
2021-07-10 17:40:47 +0200

    fix MyApp return type

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../css/_global.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />
 }
 export default MyApp
