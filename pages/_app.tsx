21443a91e18558abfda5de3fd7b1669628db31d3
Andrzej Franek <andrzejfranek@hotmail.com>
2021-07-11 21:08:53 +0200

    error page out from layout and header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,21 @@ import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 import Header from '../components/Header'
 import { Provider } from "next-auth/client";
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
+
     return (
         <Provider session={pageProps.session}>
-            <Layout>
-                <Header>
+            {Component.displayName === "ErrorPage" ?
+                <Component {...pageProps} /> :
+                <Layout>
+                    <Header>
 
-                    <Component {...pageProps} />
+                        <Component {...pageProps} />
 
-                </Header>
-            </Layout>
+                    </Header>
+                </Layout>
+            }
+
         </Provider >)
 }
 export default MyApp
