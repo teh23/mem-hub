689ff6a8abf074e454a11235ee1af2946dfd497d
Andrzej Franek <andrzejfranek@hotmail.com>
2021-07-11 19:46:26 +0200

    add tabs highlight dependent on path

--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 const Tabs = () => {
+    const currentRoute = useRouter().pathname.substring(1)
     const tabName = ['Hot', 'Latest']
     return (
         <div className="container w-full h-10 border-b border-gray-600 flex">
             {tabName.map((item, index) => {
                 return (
-                    <div key={item} className="h-full w-full flex justify-center items-center hover:border-b-2 hover:text-white text-sm text-gray-400 box-content">
-                        <Link href={`/${item.toLowerCase()}`}><a >{item}</a></Link>
-                    </div>
+                    <Link key={item} href={`/${item.toLowerCase()}`} passHref>
+                        <div className={`h-full w-full flex justify-center items-center text-sm  box-content ${currentRoute === item.toLowerCase() ? `text-white border-b-2` : 'text-gray-400 '}  `}>
+                            <a >{item}</a>
+                        </div>
+                    </Link>
                 )
             })}
 
