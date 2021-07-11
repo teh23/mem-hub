4ce552cbb7dcb700a5be605133ab6a65da5d9b48
Andrzej Franek <andrzejfranek@hotmail.com>
2021-07-11 18:53:24 +0200

    add destrucing props in button component

--- components/ButtonBorder.tsx
+++ components/ButtonBorder.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ButtonBorder = ({ className, text, ...props }: { className?: String, text: String }) => {
 
     return (
-        <button className={`border rounded-lg py-1 px-10 ${className}`}>{text.toUpperCase()}</button>
+        <button {...props} className={`border rounded-lg py-1 px-10 ${className}`}> {text.toUpperCase()}</button >
     )
 }
 
