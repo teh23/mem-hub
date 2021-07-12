3391ff21b4b341782a7375ee0bb8ce383df6a25b
Andrzej Franek <andrzejfranek@hotmail.com>
2021-07-12 19:14:03 +0200

    add post compponent

---
+++ components/Post.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react'
+
+interface IItem {
+    id: number,
+    title: string,
+    image: string,
+    createAt: Date,
+    authorId: number,
+    comments: IComments[]
+}
+interface IComments {
+    id: number,
+    postId: number,
+    userId: number,
+    comment: string
+}
+
+
+const Post = ({ item }: { item: IItem }) => {
+    const { id, title, image, createAt, authorId, comments } = item
+    console.log(comments)
+    return (
+        <div key={id} className="space-y-5 md:max-w-4xl">
+            <div>
+                <h1 className=" text-2xl font-medium">{title}</h1>
+                <div className="flex justify-between">
+                    <small className="text-gray-300">#tags #tags #tags</small>
+                    <small className="text-gray-400">{createAt.getUTCDay()} day ago</small>
+                </div>
+            </div>
+            <div className="w-full bg-black bg-opacity-90 object-center flex justify-center"><img alt={title} src={image} className=" " /></div>
+
+        </div>
+    )
+}
+
+export default Post
