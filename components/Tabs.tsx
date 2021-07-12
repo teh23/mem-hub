721ddc9c0edc49797e7f4f9329a2ffa3ba1aedef
Andrzej Franek <andrzejfranek@hotmail.com>
2021-07-12 16:53:34 +0200

    rewrite tab component

--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
 const Tabs = () => {
     const currentRoute = useRouter().pathname.substring(1)
+
+
+
     const tabName = ['Hot', 'Latest']
     return (
-        <div className="container w-full h-10 border-b border-gray-600 flex">
+        <div className="container flex-start bg-transparent md:w-6/12 h-10  flex">
             {tabName.map((item, index) => {
                 return (
                     <Link key={item} href={`/${item.toLowerCase()}`} passHref>
-                        <div className={`h-full w-full flex justify-center items-center text-sm  cursor-pointer box-content ${currentRoute === item.toLowerCase() ? `text-white border-b-2` : 'text-gray-400 '}  `}>
+                        <div className={`h-full w-full flex justify-center items-center text-sm  cursor-pointer box-content ${currentRoute === item.toLowerCase() || (item === 'Hot' && currentRoute === '') ? `border-b-0 bg-white text-black font-bold rounded-md` : 'text-gray-400 '}  `}>
                             <a >{item}</a>
                         </div>
                     </Link>
@@ -18,7 +21,7 @@ const Tabs = () => {
             })}
 
 
-        </div>
+        </div >
     )
 }
 
