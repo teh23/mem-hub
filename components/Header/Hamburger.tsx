b68499f717c3e273144f20cba5ccc29fc6ebc601
Andrzej Franek <andrzejfranek@hotmail.com>
2021-07-12 16:53:53 +0200

    hidde hamburger

--- components/Header/Hamburger.tsx
+++ components/Header/Hamburger.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import Image from 'next/image'
-import hamburger from '../../public/hamburger.svg'
+// import React from 'react'
+// import Image from 'next/image'
+// import hamburger from '../../public/hamburger.svg'
 
-const Hamburger = ({ setHidden, hidden }: {
-    setHidden: React.Dispatch<React.SetStateAction<boolean>>,
-    hidden: boolean
-}) => {
-    return (
-        <div className={`h-6 w-6 `} onClick={() => { setHidden(!hidden) }}>
-            <Image src={hamburger} alt="Logo" layout="responsive"
-            />
-        </div>
-    )
-}
+// const Hamburger = ({ setHidden, hidden }: {
+//     setHidden: React.Dispatch<React.SetStateAction<boolean>>,
+//     hidden: boolean
+// }) => {
+//     return (
+//         <div className={`h-6 w-6 md:hidden`} onClick={() => { setHidden(!hidden) }}>
+//             <Image src={hamburger} alt="Logo" layout="responsive"
+//             />
+//         </div>
+//     )
+// }
 
-export default Hamburger
+// export default Hamburger
