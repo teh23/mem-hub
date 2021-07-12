d0fde4268985dbfce567317a6cf90745737e0ce3
Andrzej Franek <andrzejfranek@hotmail.com>
2021-07-12 19:15:14 +0200

    change default page to /hot

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Layout from '../components/Layout'
 import Header from '../components/Header'
 import { InferGetStaticPropsType } from "next";
 import Main from '../components/Main';
-import Hot from './hot'
+
 interface IUser {
     id: Number
     username: String
@@ -12,23 +12,12 @@ interface IUser {
     createAt: Date
 }
 
-export default function Home({ users }: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function Home(props) {
     return (
         <>
-            <Hot />
+
         </>
     )
 }
 
-export async function getStaticProps() {
-    const prisma = new PrismaClient()
-    const user = await prisma.user.findMany()
 
-    return {
-        props: {
-            users: user
-        }
-
-    }
-
-}
