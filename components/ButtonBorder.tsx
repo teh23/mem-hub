6bce547d5234de0a5a0ffdb4f1c1855943f3df00
Andrzej Franek <andrzejfranek@hotmail.com>
2021-07-12 03:08:38 +0200

    fix buttonBorder

--- components/ButtonBorder.tsx
+++ components/ButtonBorder.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-const ButtonBorder = ({ className, text, ...props }: { className?: String, text: String }) => {
+interface IButtonBorder
+    extends React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+    > {
 
+}
+
+const ButtonBorder = (props: IButtonBorder) => {
+    const { className, children } = props
     return (
-        <button {...props} className={`border rounded-lg py-1 px-10 ${className}`}> {text.toUpperCase()}</button >
+        <button {...props} className={`border rounded-lg py-1 px-10 ${className}`}> {children}</button >
     )
 }
 
