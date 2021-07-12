8700d75bc0e57a4f938f629a945930ed5fb32d97
Andrzej Franek <andrzejfranek@hotmail.com>
2021-07-12 16:51:30 +0200

    rewrite header comp; add flex to layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,25 +4,27 @@ import Image from 'next/image'
 import Hamburger from './Header/Hamburger'
 import Menu from './Menu'
 import Tabs from './Tabs'
-
+import { useEffect, useMemo } from 'react'
+import useWindowDimensions from './Hooks/useWindowDimensions'
+import Nav from './Nav'
 
 
 const Header = ({ children }: { children: React.ReactElement }) => {
     const [hidden, setHidden] = useState(true)
     const list = ['About me', 'Next', 'Test', 'Dodaj', 'Cos nowego']
 
+
     return (
         <>
-            <header className=" h-full  text-white  bg-black bg-opacity-80 font-normal">
-                <div className="container px-4 py-2 flex justify-between items-center">
+            <header className=" h-full w-full md:w-full text-white  bg-black bg-opacity-80 font-normal">
+                <div className="container m-auto px-4 py-2 flex justify-between items-center">
                     <Logo />
 
-                    <Hamburger hidden={hidden} setHidden={setHidden} />
-
-                    <Menu hidden={hidden} setHidden={setHidden} menuItems={list} />
+                    <Nav />
                 </div>
-                <Tabs />
+
             </header >
+
             {children}
         </>
     )
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Layout = ({ children }: { children: React.ReactElement }) => {
     return (
-        <div className="container min-h-screen bg-black bg-opacity-90">
+        <div className=" flex flex-col justify-start min-h-screen bg-black bg-opacity-90 m-0">
             {children}
         </div>
     )
