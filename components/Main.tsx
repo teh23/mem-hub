775b1579e7512a24ec3e4550888687206c290135
Andrzej Franek <andrzejfranek@hotmail.com>
2021-07-12 16:52:10 +0200

    add main component; hidde menu comonent

---
+++ components/Main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Tabs from './Tabs'
+
+const Main = ({ children }: { children: JSX.Element[] | JSX.Element }) => {
+    return (
+        <div className="space-y-4 mt-4 md:space-x-4 md:space-y-0 container mx-auto rounded-md flex-col flex md:flex-row justify-between">
+            <div className="bg-black bg-opacity-50 px-5 py-3 md:p-5 rounded-md  flex-grow text-white max-h-15">tags</div>
+            <div className="bg-black text-white bg-opacity-50 p-5 rounded-md space-y-4  flex-grow">
+                <Tabs />{children}</div>
+            <div className="flex-grow rounded-md invisible  hidden md:block"></div>
+
+        </div>
+    )
+}
+
+export default Main
--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,53 +1,53 @@
-import React from 'react'
-import ButtonBorder from './ButtonBorder'
-import Overlay from './Overlay'
-import { signIn, signOut, useSession } from "next-auth/client";
-import { useRouter } from 'next/router'
-const Menu = ({ setHidden, hidden, menuItems }: {
-    setHidden: React.Dispatch<React.SetStateAction<boolean>>,
-    hidden: boolean,
-    menuItems: String[]
-}) => {
-    const [session, loading] = useSession();
-    const router = useRouter()
-    if (loading) {
-        return <p>Loading...</p>
-    }
+    // import React from 'react'
+    // import ButtonBorder from './ButtonBorder'
+    // import Overlay from './Overlay'
+    // import { signIn, signOut, useSession } from "next-auth/client";
+    // import { useRouter } from 'next/router'
+    // const Menu = ({ setHidden, hidden, menuItems }: {
+    //     setHidden: React.Dispatch<React.SetStateAction<boolean>>,
+    //     hidden: boolean,
+    //     menuItems: String[]
+    // }) => {
+    //     const [session, loading] = useSession();
+    //     const router = useRouter()
+    //     if (loading) {
+    //         return <p>Loading...</p>
+    //     }
 
-    return (
-        <>
-            <Overlay hidden={hidden} setHidden={setHidden} />
+    //     return (
+    //         <>
+    //             <Overlay className="md:hidden" hidden={hidden} setHidden={setHidden} />
 
-            <nav className={`absolute inset-y-0 right-0 h-screen w-8/12 bg-opacity-90 backdrop-filter backdrop-blur-sm bg-black ${hidden ? 'hidden' : ''} `} >
-                <div className="w-full h-1/12 pb-4 pt-4 flex justify-center flex-col items-center space-y-6 text-gray-400 pattern rounded-lg">
+    //             <nav className={`absolute inset-y-0 right-0 h-screen w-8/12 bg-opacity-90 backdrop-filter backdrop-blur-sm bg-black ${hidden ? 'hidden' : ''} `} >
+    //                 <div className="w-full h-1/12 pb-4 pt-4 flex justify-center flex-col items-center space-y-6 text-gray-400 pattern rounded-lg">
 
-                    <div className="w-20 h-20 rounded-full bg-gray-50">
+    //                     <div className="w-20 h-20 rounded-full bg-gray-50">
 
-                    </div>
-                    <p>Hello, username</p>
-                </div>
-                <ul className="flex flex-col justify-center ">
-                    {menuItems.map((key, index) => {
-                        return (
+    //                     </div>
+    //                     <p>Hello, username</p>
+    //                 </div>
+    //                 <ul className="flex flex-col justify-center ">
+    //                     {menuItems.map((key, index) => {
+    //                         return (
 
-                            <li className="mb-2 pt-3 pb-3 pl-4 rounded-lg font-light hover:bg-gray-400 hover:bg-opacity-20 flex cursor-pointer active:none" key={index}>
+    //                             <li className="mb-2 pt-3 pb-3 pl-4 rounded-lg font-light hover:bg-gray-400 hover:bg-opacity-20 flex cursor-pointer active:none" key={index}>
 
-                                {key}
-                            </li>
+    //                                 {key}
+    //                             </li>
 
-                        )
-                    })}
-                </ul>
-                {session ?
-                    <ButtonBorder onClick={() => signOut()} className={`absolute  mx-auto my-5 inset-x-0 bottom-0`} >log out</ButtonBorder> :
-                    <ButtonBorder onClick={() => signIn()} className={`absolute  mx-auto my-5 inset-x-0 bottom-0`}>log in</ButtonBorder>
+    //                         )
+    //                     })}
+    //                 </ul>
+    //                 {session ?
+    //                     <ButtonBorder onClick={() => signOut()} className={`absolute  mx-auto my-5 inset-x-0 bottom-0`} >log out</ButtonBorder> :
+    //                     <ButtonBorder onClick={() => signIn()} className={`absolute  mx-auto my-5 inset-x-0 bottom-0`}>log in</ButtonBorder>
 
-                }
+    //                 }
 
-            </nav>
+    //             </nav>
 
-        </>
-    )
-}
+    //         </>
+    //     )
+    // }
 
-export default Menu
+    // export default Menu
